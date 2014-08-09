2216378c087080b2c58f5e384a6efe437c4a579b
vishal singh <vishalsingh@static-9.55.98.14-tataidc.co.in>
2014-08-10 01:10:11 +0530

    add refferal count

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register User do
-  actions :index, :show
+	# index do
+ #    column "Reffrals" do |user|
+ #       user.referrals.count
+ #    end
+ index do                            
+    column :id                     
+    column :email        
+    column :referral_code
+    column "Reffrals" do |user|
+      user.referrals.count
+    end           
+    column :created_at 
+    column :updated_at             
+
+    default_actions                   
+  end  
+  #actions :index, :show
 end
