743de3cd0cb0e2b22389c782aac56367b148afd6
vishal singh <vishalsingh@static-242.223.96.14-tataidc.co.in>
2014-08-09 23:24:28 +0530

    set upimages on email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Prelaunchr::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
+   config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   # For mailer configs
   config.action_mailer.perform_deliveries = true
