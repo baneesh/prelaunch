285f6dadcec73bbe85b9824fd2b7733fbcac0b60
vishal singh <vishalsingh@vishals-MacBook-Pro.local>
2014-08-11 12:52:18 +0530

    secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || 'xxxxxxxx-change-me-please-xxxxxxxx'
+Prelaunchr::Application.config.secret_token = 'd90740c84250e0fbfbffa29a27be9dd80b95335c93b824b90a82a02b7ce37394422dd124cd8b198d561e2bdb14480bbb621b0a43602b9fb1907fe9a1776a0527'
