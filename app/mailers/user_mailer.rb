ce4034163d32ea748a993557261d48dcf24e4d2e
Aneesh Seth <baneesh87@gmail.com>
2014-08-20 12:22:56 +0530

    twitter trailing and message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
     def signup_email(user)
         @user = user
-        @twitter_message = "Luxury is evolving - didn't want to leave you behind! Check out 1701 here, and sign up to earn free products."
+        @twitter_message = "Luxury is evolving - didn't want to leave you behind! Check out @workshop1701 here, and sign up to earn free products!"
 
         mail(:to => user.email, :subject => "Thanks for signing up!")
     end
