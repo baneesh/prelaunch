77b4dfb108dd8072561a2a3911ce684af3ad3934
vishal singh <vishalsingh@vishals-MacBook-Pro.local>
2014-10-08 13:52:05 +0530

    csv export with reffrals

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -3,6 +3,16 @@ ActiveAdmin.register User do
  #    column "Reffrals" do |user|
  #       user.referrals.count
  #    end
+
+ csv do
+    column :id
+    column('Email') { |user| user.email }
+    column('Referral Code') { |user| user.referral_code }
+    column('Referrals') { |user|  user.referrals.count }
+    column('Created At') { |user| user.created_at }
+    column('Updated At') { |user| user.updated_at }
+  end
+
  index do                            
     column :id                     
     column :email        
